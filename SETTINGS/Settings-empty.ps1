dd0b405854de6486bb445345b980bd8c500a4ec9
Alex-on-AB <1928311@tuta.io>
2020-08-17 00:29:53 +0300

    Changed git ignore.

--- SETTINGS/Settings-empty.ps1
+++ SETTINGS/Settings-empty.ps1
@@ -1,13 +1,15 @@
 # Rename this file to Settings.ps1
+######################### value replacement #####################
+    
+
 ######################### no replacement ########################
-    [array] $global:IgnoreFolders                    = @("*.vscode")         # Ignored folders names.
-    [array] $global:FoldersToApplyPath               = @($Global:ProjectsFolderPath, $Global:ProjectServicesFolderPath)         # Folders where searching for Setting*.ps1 files.
+    [array] $global:IgnoreFolders                    = @($Global:TemplateProjectPath)                    # Ignored folders names.
+    [array] $global:FoldersToApplyPath               = @($Global:WorkFolderList)                         # Folders where searching for Setting*.ps1 files.
     [string]$global:NewFileNameEnd                   = "-empty.ps1"                                      # End of the new file name.
     [string]$global:NoReplacementSection             = "# no replacement #"                              # Copy as is below this line.
     [string]$global:LocalSection                     = "# local section #"                               # Skip below this line.
     [string]$global:ValueReplacementSection          = "# value replacement #"                           # Replace values to null below this line.
     [string]$global:EmptySettingsStub                = "# Rename this file to Settings.ps1"
-    
     [bool]  $Global:LocalSettingsSuccessfullyLoaded  = $true
 
 # Error trap
